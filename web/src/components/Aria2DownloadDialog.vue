b46817e9db03101cbe8e0837de31a55ccc739ba4
CodeingBoy <CodeingBoy@gmail.com>
2020-02-08 17:31:34 +0800

    Fetch folders concurrently

--- web/src/components/Aria2DownloadDialog.vue
+++ web/src/components/Aria2DownloadDialog.vue
@@ -1,288 +1,252 @@
 <template>
-	<v-dialog v-model="show" :persistent="true" max-width="700">
-		<template v-slot:activator="{ on }">
-			<v-btn
-				v-if="enabled"
-				v-text="$t('aria2Download')"
-				color="primary"
-				@click="onShow"
-				id="btn-show"
-			></v-btn>
-		</template>
-		<v-card>
-			<v-card-title class="headline">
-				<span>{{ $t('aria2Download') }}</span>
-			</v-card-title>
-			<v-card-text>
-				<v-container>
-					<v-row>
-						<v-text-field
-							:label="$t('aria2RPCDownloadPath')"
-							v-model="downloadPath"
-						></v-text-field>
-					</v-row>
-					<v-row>
-						<v-checkbox
-							:label="$t('aria2DownloadRecursive')"
-							v-model="recursive"
-							:disabled="!canOperate"
-						></v-checkbox>
-					</v-row>
-				</v-container>
-				<template v-if="status === 1">
-					<v-alert dense type="info">
-						{{
-							$t('aria2FetchingFiles', {
-								remainingFolderCount: fetchRemainingFolderCount
-							})
-						}}
-					</v-alert>
-				</template>
-				<template v-if="status === 2">
-					<v-alert dense type="info">
-						{{
-							$t('aria2DownloadStats', {
-								fileCount: files.length,
-								folderCount: folders.length,
-								totalDownloadSize
-							})
-						}}
-					</v-alert>
-				</template>
-				<template v-if="status === 3">
-					<v-alert dense type="info">
-						{{
-							$t('aria2DownloadProgress', {
-								remainingFileCount
-							})
-						}}
-					</v-alert>
-				</template>
-				<template v-if="status === 4">
-					<v-alert dense type="success">
-						{{ $t('aria2DownloadSuccess') }}
-					</v-alert>
-				</template>
-				<template v-if="status === 5">
-					<v-alert dense type="error">
-						{{ $t('aria2DownloadFailed', { failReason }) }}
-					</v-alert>
-				</template>
-				<template v-if="status === 6">
-					<v-alert dense type="error">
-						{{ $t('aria2FetchFileFailed', { failReason }) }}
-					</v-alert>
-				</template>
-				<v-alert dense type="warning" v-if="shouldShowAriaHTTPSWarning">
-					{{ $t('aria2HTTPSWarning') }}
-				</v-alert>
-			</v-card-text>
-			<v-card-actions id="actions">
-				<v-btn
-					color="primary"
-					@click="startDownload"
-					v-text="$t('start')"
-					:disabled="!canOperate"
-					text
-				></v-btn>
-				<v-btn @click="close" v-text="$t('close')" text></v-btn>
-			</v-card-actions>
-		</v-card>
-	</v-dialog>
+    <v-dialog v-model="show" :persistent="true" max-width="700">
+        <template v-slot:activator="{ on }">
+            <v-btn v-if="enabled" v-text="$t('aria2Download')" color="primary" @click="onShow" id="btn-show"></v-btn>
+        </template>
+        <v-card>
+            <v-card-title class="headline">
+                <span>{{ $t('aria2Download') }}</span>
+            </v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-text-field :label="$t('aria2RPCDownloadPath')" v-model="downloadPath"></v-text-field>
+                    </v-row>
+                    <v-row>
+                        <v-checkbox
+                            :label="$t('aria2DownloadRecursive')"
+                            v-model="recursive"
+                            :disabled="!canOperate"
+                        ></v-checkbox>
+                    </v-row>
+                </v-container>
+                <template v-if="status === 1">
+                    <v-alert dense type="info">
+                        {{
+                            $t('aria2FetchingFiles', {
+                                remainingFolderCount: fetchRemainingFolderCount
+                            })
+                        }}
+                    </v-alert>
+                </template>
+                <template v-if="status === 2">
+                    <v-alert dense type="info">
+                        {{
+                            $t('aria2DownloadStats', {
+                                fileCount: files.length,
+                                folderCount: folders.length,
+                                totalDownloadSize
+                            })
+                        }}
+                    </v-alert>
+                </template>
+                <template v-if="status === 3">
+                    <v-alert dense type="info">
+                        {{
+                            $t('aria2DownloadProgress', {
+                                remainingFileCount
+                            })
+                        }}
+                    </v-alert>
+                </template>
+                <template v-if="status === 4">
+                    <v-alert dense type="success">
+                        {{ $t('aria2DownloadSuccess') }}
+                    </v-alert>
+                </template>
+                <template v-if="status === 5">
+                    <v-alert dense type="error">
+                        {{ $t('aria2DownloadFailed', { failReason }) }}
+                    </v-alert>
+                </template>
+                <template v-if="status === 6">
+                    <v-alert dense type="error">
+                        {{ $t('aria2FetchFileFailed', { failReason }) }}
+                    </v-alert>
+                </template>
+                <v-alert dense type="warning" v-if="shouldShowAriaHTTPSWarning">
+                    {{ $t('aria2HTTPSWarning') }}
+                </v-alert>
+            </v-card-text>
+            <v-card-actions id="actions">
+                <v-btn color="primary" @click="startDownload" v-text="$t('start')" :disabled="!canOperate" text></v-btn>
+                <v-btn @click="close" v-text="$t('close')" text></v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 <script>
-import nodeUrl from 'url'
-import nodePath from 'path'
-import prettyBytes from 'pretty-bytes'
-import aria2 from '../aria2'
-import api from '../api'
-import util from '../util'
+import nodeUrl from 'url';
+import nodePath from 'path';
+import prettyBytes from 'pretty-bytes';
+import aria2 from '../aria2';
+import api from '../api';
+import util from '../util';
 
 export default {
-	props: {
-		path: String,
-		rootId: String
-	},
-	data() {
-		return {
-			show: false,
-			enabled: window.props.download_aria2,
-			downloadPath: '',
-			recursive: true,
-			canOperate: false,
-			files: [],
-			folders: [],
-			totalDownloadSize: '',
-			remainingFileCount: 0,
-			fetchRemainingFolderCount: 0,
-			status: 1,
-			failReason: '',
-			shouldShowAriaHTTPSWarning: util.shouldShowAriaHTTPSWarning()
-		}
-	},
-	watch: {
-		recursive: function() {
-			this.fetchLinks()
-		}
-	},
-	methods: {
-		onShow: function() {
-			let baseDownloadPath = aria2.getDownloadPath()
-			if (baseDownloadPath.substr(-1) === '/') {
-				baseDownloadPath = baseDownloadPath.substr(
-					0,
-					baseDownloadPath.length - 1
-				)
-			}
-			const folderDownloadPath = this.path
-			this.downloadPath = `${baseDownloadPath}${folderDownloadPath}`
-			this.show = true
-			this.fetchLinks()
-		},
-		startDownload: async function() {
-			this.status = 3
-			this.canOperate = false
+    props: {
+        path: String,
+        rootId: String
+    },
+    data() {
+        return {
+            show: false,
+            enabled: window.props.download_aria2,
+            downloadPath: '',
+            recursive: true,
+            canOperate: false,
+            files: [],
+            folders: [],
+            totalDownloadSize: '',
+            remainingFileCount: 0,
+            fetchRemainingFolderCount: 0,
+            status: 1,
+            failReason: '',
+            shouldShowAriaHTTPSWarning: util.shouldShowAriaHTTPSWarning()
+        };
+    },
+    watch: {
+        recursive: function() {
+            this.fetchLinks();
+        }
+    },
+    methods: {
+        onShow: function() {
+            let baseDownloadPath = aria2.getDownloadPath();
+            if (baseDownloadPath.substr(-1) === '/') {
+                baseDownloadPath = baseDownloadPath.substr(0, baseDownloadPath.length - 1);
+            }
+            const folderDownloadPath = this.path;
+            this.downloadPath = `${baseDownloadPath}${folderDownloadPath}`;
+            this.show = true;
+            this.fetchLinks();
+        },
+        startDownload: async function() {
+            this.status = 3;
+            this.canOperate = false;
 
-			const files = this.files
-			const downloadFileInfos = files.map(f => {
-				let path = f.path.replace(this.path, '')
-				path = nodePath.dirname(path)
-				if (path === '.') {
-					path = ''
-				}
+            const files = this.files;
+            const downloadFileInfos = files.map(f => {
+                let path = f.path.replace(this.path, '');
+                path = nodePath.dirname(path);
+                if (path === '.') {
+                    path = '';
+                }
 
-				const downloadPath = this.downloadPath + path
-				const info = {
-					name: f.fileName,
-					url: f.url,
-					downloadPath
-				}
-				return info
-			})
+                const downloadPath = this.downloadPath + path;
+                const info = {
+                    name: f.fileName,
+                    url: f.url,
+                    downloadPath
+                };
+                return info;
+            });
 
-			this.remainingFileCount = downloadFileInfos.length
-			while (downloadFileInfos.length) {
-				const f = downloadFileInfos.pop()
-				try {
-					const addResult = await aria2.addDownload(
-						f.url,
-						f.downloadPath
-					)
-				} catch (e) {
-					this.failReason = e
-					this.status = 5
-					return
-				}
-				this.remainingFileCount--
-			}
-			this.status = 4
-			this.canOperate = true
-		},
-		close: function() {
-			this.show = false
-		},
-		getFileUrl(path) {
-			const { rootId } = this.$route.query
-			let u = nodeUrl.resolve(
-				window.props.api,
-				path
-					.split('/')
-					.map(encodeURIComponent)
-					.join('/')
-			)
-			if (rootId) {
-				u += '?rootId=' + rootId
-			}
-			return u
-		},
-		fetchLinks: async function() {
-			this.canOperate = false
-			this.status = 1
+            this.remainingFileCount = downloadFileInfos.length;
+            while (downloadFileInfos.length) {
+                const f = downloadFileInfos.pop();
+                try {
+                    const addResult = await aria2.addDownload(f.url, f.downloadPath);
+                } catch (e) {
+                    this.failReason = e;
+                    this.status = 5;
+                    return;
+                }
+                this.remainingFileCount--;
+            }
+            this.status = 4;
+            this.canOperate = true;
+        },
+        close: function() {
+            this.show = false;
+        },
+        getFileUrl(path) {
+            const { rootId } = this.$route.query;
+            let u = nodeUrl.resolve(
+                window.props.api,
+                path
+                    .split('/')
+                    .map(encodeURIComponent)
+                    .join('/')
+            );
+            if (rootId) {
+                u += '?rootId=' + rootId;
+            }
+            return u;
+        },
+        fetchLinks: async function() {
+            this.canOperate = false;
+            this.status = 1;
 
-			let totalDownloadSize = 0
-			const files = []
-			const folders = [this.path]
-			const folderQueue = [this.path]
-			while (folderQueue.length !== 0) {
-				this.fetchRemainingFolderCount = folderQueue.length
-				const currentPath = folderQueue.pop()
+            let totalDownloadSize = 0;
+            const pathItems = await util.getPathItems(this.path, this.rootId, this.recursive, 3, 3, remaining => {
+                this.fetchRemainingFolderCount = remaining;
+            });
 
-				let pathItems
-				try {
-					pathItems = await this.listItems(currentPath, this.rootId)
-				} catch (e) {
-					this.failReason = e
-					this.status = 6
-					return
-				}
+            const filterRecursive = (obj, condition) => {
+                const results = [];
+                if (condition(obj)) {
+                    results.push(obj);
+                }
 
-				// add files
-				const pathFiles = pathItems.filter(f => !f.isFolder)
-				files.splice(files.length, 0, ...pathFiles)
+                if (obj.hasOwnProperty('children')) {
+                    const children = obj['children'];
+                    for (const v of Object.values(children)) {
+                        results.push(...filterRecursive(v, condition));
+                    }
+                }
+                return results;
+            };
+            const files = filterRecursive(pathItems, i => !i.hasOwnProperty('children') && !i.isFolder);
+            const folders = filterRecursive(pathItems, i => i.isFolder);
 
-				if (!this.recursive) {
-					// stop if we don't recursive or no more folders to dive
-					break
-				}
+            files.forEach(f => {
+                totalDownloadSize += f.fileRawSize;
+            });
+            this.totalDownloadSize = prettyBytes(totalDownloadSize);
 
-				// ..if dive
-				// add folder paths
-				const pathFolders = pathItems.filter(f => f.isFolder)
-				const pathFolderPaths = pathFolders.map(f => f.path)
-				// folders for stat
-				folders.splice(folders.length, 0, ...pathFolderPaths)
-				// paths for dive
-				folderQueue.splice(folderQueue.length, 0, ...pathFolderPaths)
-			}
-			files.forEach(f => {
-				totalDownloadSize += f.fileRawSize
-			})
-			this.totalDownloadSize = prettyBytes(totalDownloadSize)
+            this.files = files;
+            this.folders = folders;
 
-			this.files = files
-			this.folders = folders
-
-			this.canOperate = true
-			this.status = 2
-		},
-		listItems: async function(path, rootId) {
-			let { files } = await api
-				.post(path, {
-					method: 'POST',
-					qs: {
-						rootId
-					}
-				})
-				.json()
-			files = files.map(f => {
-				f.mimeType = f.mimeType.replace('; charset=utf-8', '')
-				const isFolder =
-					f.mimeType === 'application/vnd.google-apps.folder'
-				let resourcePath =
-					nodeUrl.resolve(path, f.name) + (isFolder ? '/' : '')
-				const url = this.getFileUrl(resourcePath)
-				const o = {
-					fileName: f.name,
-					isFolder,
-					mimeType: f.mimeType,
-					fileSize: f.size ? prettyBytes(parseInt(f.size)) : '',
-					fileRawSize: parseInt(f.size || '0'),
-					path: resourcePath,
-					url
-				}
-				return o
-			})
-			return files
-		}
-	}
-}
+            this.canOperate = true;
+            this.status = 2;
+        },
+        listItems: async function(path, rootId) {
+            let { files } = await api
+                .post(path, {
+                    method: 'POST',
+                    qs: {
+                        rootId
+                    }
+                })
+                .json();
+            files = files.map(f => {
+                f.mimeType = f.mimeType.replace('; charset=utf-8', '');
+                const isFolder = f.mimeType === 'application/vnd.google-apps.folder';
+                let resourcePath = nodeUrl.resolve(path, f.name) + (isFolder ? '/' : '');
+                const url = this.getFileUrl(resourcePath);
+                const o = {
+                    fileName: f.name,
+                    isFolder,
+                    mimeType: f.mimeType,
+                    fileSize: f.size ? prettyBytes(parseInt(f.size)) : '',
+                    fileRawSize: parseInt(f.size || '0'),
+                    path: resourcePath,
+                    url
+                };
+                return o;
+            });
+            return files;
+        }
+    }
+};
 </script>
 <style>
 #actions {
-	justify-content: flex-end;
+    justify-content: flex-end;
 }
 
 #btn-show {
-	margin-right: 10px;
+    margin-right: 10px;
 }
 </style>
--- web/src/util.js
+++ web/src/util.js
@@ -1,12 +1,158 @@
-import aria2 from './aria2'
+import aria2 from './aria2';
+import api from './api';
+import nodeUrl from 'url';
+import nodePath from 'path';
+import prettyBytes from 'pretty-bytes';
 
 const usingHTTPS = () => {
-	return window.location.protocol === 'https:'
-}
+    return window.location.protocol === 'https:';
+};
+
+const getFileUrl = (path, rootId) => {
+    let u = nodeUrl.resolve(
+        window.props.api,
+        path
+            .split('/')
+            .map(encodeURIComponent)
+            .join('/')
+    );
+    if (rootId) {
+        u += '?rootId=' + rootId;
+    }
+    return u;
+};
+
+const listItems = async (path, rootId) => {
+    let { files } = await api
+        .post(path, {
+            method: 'POST',
+            qs: {
+                rootId
+            }
+        })
+        .json();
+    files = files.map(f => {
+        f.mimeType = f.mimeType.replace('; charset=utf-8', '');
+        const isFolder = f.mimeType === 'application/vnd.google-apps.folder';
+        let resourcePath = nodeUrl.resolve(path, f.name) + (isFolder ? '/' : '');
+        const url = getFileUrl(resourcePath, rootId);
+        const o = {
+            fileName: f.name,
+            isFolder,
+            mimeType: f.mimeType,
+            fileSize: f.size ? prettyBytes(parseInt(f.size)) : '',
+            fileRawSize: parseInt(f.size || '0'),
+            path: resourcePath,
+            url
+        };
+        return o;
+    });
+    return files;
+};
+
+const getPathItems = (path, rootId, recursive = true, concurrency = 3, retry = 3, onProgressUpdate = null) => {
+    return new Promise((resolve, reject) => {
+        const result = {};
+        const queue = [{ path, result, key: path }];
+
+        const updateProgress = () => {
+            if (!onProgressUpdate) {
+                return;
+            }
+            const remainingPathCount = queue.length;
+            onProgressUpdate(remainingPathCount);
+        };
+
+        const fetchPath = async (path, result, key) => {
+            const fetchPathItems = async (path, rootId, retryTimes = 3) => {
+                try {
+                    return await listItems(path, rootId);
+                } catch (e) {
+                    if (retryTimes > 0) {
+                        console.log(`Fetch path ${path} data failed, retrying`);
+                        return fetchPathItems(path, rootId, retryTimes - 1);
+                    }
+                    throw e;
+                }
+            };
+
+            const pathItems = await fetchPathItems(path, rootId, retry);
+
+            // construct results
+            const folders = pathItems.filter(f => f.isFolder);
+            const files = pathItems.filter(f => !f.isFolder);
+
+            if (!result[key]) {
+                result[key] = {};
+            }
+            if (!result[key]['children']) {
+                result[key]['children'] = {};
+            }
+            for (const i of pathItems) {
+                const name = i.fileName;
+                result[key]['children'][name] = i;
+            }
+
+            if (!recursive) {
+                // stop if we don't recursive or no more folders to dive
+                return;
+            }
+
+            // ..if dive
+            // add folder paths
+            const queuePaths = folders.map(f => {
+                const basename = nodePath.basename(f.path);
+                return {
+                    path: f.path,
+                    result: result[key]['children'],
+                    key: basename
+                };
+            });
+            // add folders to queue
+            queue.push(...queuePaths);
+        };
+
+        const promises = {};
+        const onSuccess = () => {
+            resolve(result[path]);
+        };
+        const onError = e => {
+            reject(e);
+        };
+        let id = 1;
+        const scheduleTasks = () => {
+            while (Object.keys(promises).length < concurrency && queue.length > 0) {
+                const { path, result, key } = queue.shift();
+                const taskId = id;
+                id++;
+                const p = fetchPath(path, result, key);
+                promises[taskId] = p;
+                p.then(
+                    () => {
+                        delete promises[taskId];
+                        const promiseCount = Object.keys(promises).length;
+                        updateProgress();
+                        if (queue.length == 0 && promiseCount == 0) {
+                            onSuccess();
+                        } else {
+                            scheduleTasks();
+                        }
+                    },
+                    e => {
+                        onError(e);
+                    }
+                );
+            }
+        };
+
+        scheduleTasks();
+    });
+};
 
 export default {
-	usingHTTPS,
-	shouldShowAriaHTTPSWarning: () => {
-		return usingHTTPS() && !aria2.getRpcSecure()
-	}
-}
+    usingHTTPS,
+    getPathItems,
+    shouldShowAriaHTTPSWarning: () => {
+        return usingHTTPS() && !aria2.getRpcSecure();
+    }
+};
